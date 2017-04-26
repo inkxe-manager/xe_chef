2e94469d70237d62b43ee722fd82192a17ab1358
inkxe-manager <binamramohanty@gmail.com>
2017-04-26 18:36:25 +0530

    - Deleted files

--- cookbooks/lamp/attributes/default.rb
+++
@@ -1,5 +0,0 @@
-default['lamp']['web']['document_root'] = '/var/www/default/public_html'
-
-
-default['lamp']['database']['dbname'] = 'my_company'
-default['lamp']['database']['admin_username'] = 'db_admin'
