7f7e3b7106848061fdefd47ca8763cf06e45bc36
inkxe-manager <binamramohanty@gmail.com>
2017-04-27 19:42:04 +0530

    - Databag logs

--- lamp/recipes/default.rb
+++ lamp/recipes/default.rb
@@ -13,5 +13,15 @@ app = search("aws_opsworks_app").first
 doc_root = app['attributes']['document_root']
 node.default['lamp']['web']['document_root'] = doc_root
 
+instance = search("aws_opsworks_instance").first
+layer = search("aws_opsworks_layer").first
+stack = search("aws_opsworks_stack").first
+
+Chef::Log.info("********** This instance's instance ID is '#{instance['instance_id']}' **********")
+Chef::Log.info("********** This instance's public IP address is '#{instance['public_ip']}' **********")
+Chef::Log.info("********** This instance belongs to the layer '#{layer['name']}' **********")
+Chef::Log.info("********** This instance belongs to the stack '#{stack['name']}' **********")
+Chef::Log.info("********** This stack gets its cookbooks from '#{stack['custom_cookbooks_source']['url']}' **********")
+
 include_recipe 'lamp::web'
 include_recipe 'lamp::database'
