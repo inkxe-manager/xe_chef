cd95bce7f9920ec634b16b1ddd9cef2014146d03
inkxe-manager <binamramohanty@gmail.com>
2017-04-27 16:18:58 +0530

    - Changes to settings

--- lamp/attributes/default.rb
+++ lamp/attributes/default.rb
@@ -1,4 +1,8 @@
-default['lamp']['web']['document_root'] = '/var/www/default/public_html'
+app = search("aws_opsworks_app").first
+doc_root= app['attributes']['document_root']
+
+
+default['lamp']['web']['document_root'] = doc_root
 
 
 default['lamp']['database']['dbname'] = 'my_company'
--- lamp_customers/attributes/default.rb
+++ lamp_customers/attributes/default.rb
@@ -1,2 +1,5 @@
-default['lamp']['web']['document_root'] = '/var/www/customers/public_html'
+app = search("aws_opsworks_app").first
+doc_root_lamp_cust = app['attributes']['document_root']
+
+default['lamp']['web']['document_root'] = doc_root_lamp_cust
 default['lamp']['database']['dbname'] = '4thcoffee'
